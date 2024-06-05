31e73b85b794dce4b02f0ad0d8ef913d30ad9268
tantriklusi <tantriklusiana@gmail.com>
2024-06-05 21:57:00 +0700

    revisi tampilan

--- src/Components/detailtiket.jsx
+++ src/Components/detailtiket.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-// Color palette 00B7C2
 import React, { useState } from "react";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { PiAirplaneTakeoffFill, PiAirplaneLandingFill, PiWifiXBold } from "react-icons/pi";
@@ -13,6 +11,24 @@ import { useNavigate } from "react-router-dom";
 export function DetailTiket() {
   const navigate = useNavigate();
 
+  const detailtiket = {
+    namaMaskapai: "Citilink",
+    kelas: "Ekonomi",
+    tanggal: "10 Mei 2024",
+    waktuKeberangkatan: "07.00",
+    waktuKedatangan: "08.55",
+    kotaAsal: "Jakarta(CGK)",
+    kotaTujuan: "Denpasar(DPS)",
+    bandaraAsal: "Soekarno-Hatta International Airport (CGK)",
+    bandaraTujuan: "Ngurah Rai International Airport (DPS)",
+    durasi: "1jam 55menit",
+    harga: "Rp 792.656",
+    cabin: "max 7kg",
+    informasi: {
+      fasilitas: "air mineral",
+    },
+  };
+
   return (
     <div className=" w-full">
       <h1 className="text-2xl font-bold mb-4 mt-3 border-b text-center">Detail Penerbangan</h1>
@@ -20,7 +36,7 @@ export function DetailTiket() {
         <div className="flex flex-col md:flex-row items-center w-full space-y-4 md:space-y-0 md:space-x-4 text-white font-semibold">
           <h1 className="rounded-md bg-[#00B7C2] p-2 w-full flex items-center text-left">
             <IoMdArrowRoundBack className="mr-2" />
-            Jakarta (CGK) ke Denpasar (DPS) - Bali
+            {detailtiket.kotaAsal} ke {detailtiket.kotaTujuan}
           </h1>
           <button type="submit" className="text-center rounded-md bg-gray-800 p-2 w-full md:w-auto" onClick={() => navigate("/pencarian")}>
             Ubah Pencarian
@@ -31,11 +47,10 @@ export function DetailTiket() {
       <div className="flight-ticket-card shadow-lg border-[#00B7C2] border rounded-md px-6 py-4 text-sm text-black dark:text-white font-bold w-full my-4 mx-auto border-opacity-50 max-w-7xl">
         <div className="flex flex-col md:flex-row items-center justify-between py-4">
           <h2 className="flex items-center text-base font-bold tracking-tight">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f5/AirAsia_New_Logo.svg" className="h-6 mr-2" alt="Airasia Logo" />
-            AirAsia Indonesia
+            <img src="https://logowik.com/content/uploads/images/citilink3703.logowik.com.webp" className="h-8 w-8 mr-2 rounded-full" alt="Citilink Logo" /> {detailtiket.namaMaskapai}
           </h2>
           <div className="flex items-center">
-            <h1 className="text-blue-700 dark:text-gray-400 font-bold mr-1">RP 972.600</h1>
+            <h1 className="text-blue-700 dark:text-gray-400 font-bold mr-1">{detailtiket.harga}</h1>
             <p className="text-gray-500 font-semibold">/orang</p>
           </div>
         </div>
@@ -43,20 +58,20 @@ export function DetailTiket() {
           <div>
             <div className="font-bold text-gray-700 dark:text-gray-400">Detail Penerbangan</div>
             <h1 className="text-gray-700 dark:text-gray-400 font-semibold mb-2 p-1 flex items-center">
-              <MdAirlineSeatReclineNormal className="mr-2" /> Ekonomi | CT123
+              <MdAirlineSeatReclineNormal className="mr-2" /> {detailtiket.kelas}
             </h1>
             <h1 className="text-gray-700 dark:text-gray-400 font-semibold mb-2 p-1 flex items-center">
-              <GoClockFill className="mr-2" /> 10 Mei 2024 | 20:00 - 22:55
+              <GoClockFill className="mr-2" /> {detailtiket.tanggal} | {detailtiket.waktuKeberangkatan} - {detailtiket.waktuKedatangan}
             </h1>
             <h1 className="text-gray-700 dark:text-gray-400 font-semibold mb-2 p-1 flex items-center">
-              <PiAirplaneTakeoffFill className="mr-2" /> Soekarno-Hatta International Airport (CGK)
+              <PiAirplaneTakeoffFill className="mr-2" /> {detailtiket.bandaraAsal}
             </h1>
             <h1 className="text-gray-700 dark:text-gray-400 font-semibold mb-2 p-1 flex items-center">
-              <PiAirplaneLandingFill className="mr-2" /> Ngurah Rai International Airport (DPS)
+              <PiAirplaneLandingFill className="mr-2" /> {detailtiket.bandaraTujuan}
             </h1>
             <h1 className="text-gray-700 dark:text-gray-400 font-semibold mb-2 p-1 flex items-center">
               <LuBaggageClaim className="mr-2" />
-              Cabin Baggage: Max 7kg
+              Cabin Baggage: {detailtiket.cabin}
             </h1>
           </div>
         </div>
--- src/Components/pencarian.jsx
+++ src/Components/pencarian.jsx
@@ -1,11 +1,12 @@
-/* eslint-disable no-unused-vars */
-// Color palette 00B7C2
 import React, { useState } from "react";
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { TfiMoney } from "react-icons/tfi";
 import { PiHeartFill } from "react-icons/pi";
+import { useSpring, animated } from "react-spring";
+import { FaPlaneDeparture } from "react-icons/fa";
 
 export function Pencarian() {
   // const [isLoading, setIsLoading] = useState(false);
@@ -15,9 +16,31 @@ export function Pencarian() {
   const days = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
   const today = new Date();
 
+  const tiket = {
+    namaMaskapai: "Citilink",
+    kelas: "Ekonomi",
+    waktuKeberangkatan: "07.00",
+    waktuKedatangan: "08.55",
+    bandaraAsal: "Jakarta(CGK)",
+    bandaraTujuan: "Denpasar(DPS)",
+    durasi: "1jam 55menit",
+    harga: "Rp 792.656",
+    informasi: {
+      penumpang: "2",
+      fasilitas: "air mineral",
+    },
+  };
+
   const handleTabClick = (index) => {
     setActiveTab(index);
   };
+
+  const planeAnimation = useSpring({
+    loop: true,
+    to: [{ transform: "translateX(10px)" }, { transform: "translateX(0px)" }],
+    from: { transform: "translateX(0px)" },
+    config: { duration: 1000 },
+  });
 
   // const handlePilihClick = () => {
   //   // Tampilkan spinner ketika tombol Pilih diklik
@@ -39,11 +62,8 @@ export function Pencarian() {
         <div className="flex flex-col md:flex-row items-center w-full space-y-4 md:space-y-0 md:space-x-4 text-white font-semibold">
           <h1 className="rounded-md bg-[#00B7C2] p-2 w-full flex items-center text-left">
             <IoMdArrowRoundBack className="mr-2" />
-            Jakarta (CGK) ke Denpasar (DPS) - 2 Penumpang - Ekonomi
+            {tiket.bandaraAsal} ke {tiket.bandaraTujuan} - {tiket.informasi.penumpang} Penumpang - {tiket.kelas}
           </h1>
-          {/* <button type="submit" className="text-center rounded-md bg-[#195d61] p-2 w-full md:w-auto">
-            Ubah Pencarian
-          </button> */}
           <select className="select-cst text-center rounded-md bg-gray-800 p-2 w-full md:w-auto">
             <option value="" disabled selected hidden>
               Urutkan Berdasarkan
@@ -94,93 +114,67 @@ export function Pencarian() {
 
           <TabPanel>
             <div className={`p-4 my-4 border border-gray-100 dark:border-gray-700 rounded-md max-w-7xl items-center justify-between ${activeTab === 0 ? "block" : "hidden"} mx-auto`}>
-              <div className="flight-ticket-card shadow-lg border-[#00B7C2] border-2 rounded-md px-4 py-2 text-white text-left font-bold w-full my-3">
-                <h2 className="flex items-center text-lg font-bold tracking-tight text-black dark:text-white">
-                  <img src="https://logowik.com/content/uploads/images/citilink3703.logowik.com.webp" className="h-8 mr-2" alt="citilink Logo" />
-                  Citilink
-                </h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                  <div>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold">Ekonomi</h1>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold">Departure: 20:00</h1>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold">Arrival: 22:55</h1>
+              <div className="mx-8 bg-white shadow-lg rounded-lg outline-1 mb-4">
+                <div className="bg-gray-400 text-white py-2 px-4 rounded-t-lg">
+                  <p className="flex items-center text-lg font-bold tracking-tight">
+                    <img src="https://logowik.com/content/uploads/images/citilink3703.logowik.com.webp" className="h-8 w-8 mr-2 rounded-full" alt="Citilink Logo" />
+                    {tiket.namaMaskapai} - {tiket.kelas}
+                  </p>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 py-4">
+                  <div className="flex flex-col items-center">
+                    <span className="text-gray-700 dark:text-gray-400 font-semibold">{tiket.waktuKeberangkatan}</span>
+                    <span className="text-gray-700 dark:text-gray-400 font-semibold">{tiket.bandaraAsal}</span>{" "}
                   </div>
-                  <div className="flex justify-between items-center">
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold border-b border-gray-700">1j 55mnt</h1>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-bold">RP 792.656/org</h1>
+                  <div className="flex flex-col items-center col-span-2">
+                    <animated.div style={planeAnimation} className="flex flex-col items-center">
+                      <FaPlaneDeparture size={30} className="text-[#00B7C2]" />
+                      <span className="text-gray-700 dark:text-gray-400 font-semibold mt-2 flex items-center">{tiket.durasi}</span>
+                    </animated.div>
+                  </div>
+                  <div className="flex flex-col items-center">
+                    <span className="text-gray-700 dark:text-gray-400 font-semibold">{tiket.waktuKedatangan}</span>
+                    <span className="text-gray-700 dark:text-gray-400 font-semibold">{tiket.bandaraTujuan}</span>
                   </div>
                 </div>
-                <div className="flex justify-end mt-4">
-                  <button className="text-white bg-[#00B7C2] hover:bg-[#0f5c60] px-4 py-2 rounded-md" onClick={() => navigate("/detailtiket")}>
+                <div className="flex flex-col sm:flex-row items-center sm:justify-end sm:items-center p-4 ">
+                  <span className="text-[#00B7C2] text-lg font-bold mt-2 sm:mt-0">{tiket.harga}</span>
+                  <Link to="/detailtiket" className="block text-center bg-gray-600 hover:bg-gray-800 text-white font-bold text-l py-2 px-4 rounded-md focus:outline-none sm:ml-4 mt-4 sm:mt-0">
                     Lihat
-                  </button>
+                  </Link>
                 </div>
               </div>
 
-              <div className="flight-ticket-card shadow-lg border-[#00B7C2] border-2 rounded-md px-4 py-2 text-white text-left font-bold w-full">
-                <h2 className="flex items-center text-lg font-bold tracking-tight text-black dark:text-white">
-                  <img src="https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2022/11/26/451507063.jpg" className="h-8 mr-2" alt="Airline Logo" />
-                  Super Air Jet
-                </h2>
-                <div className="flex justify-between items-center ">
-                  <div>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold">Ekonomi</h1>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold">Departure: 18:45 </h1>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold">Arrival: 21:40</h1>
-                  </div>
-                  <h1 className="text-gray-700 dark:text-gray-400 font-semibold border-b border-gray-700">1j 55mnt</h1>
-                  <h1 className="text-gray-700 dark:text-gray-400 font-bold">RP 909.900/org</h1>
+              <div className="mx-8 bg-white shadow-lg rounded-lg outline-1 mb-4">
+                <div className="bg-gray-400 text-white py-2 px-4 rounded-t-lg">
+                  <p className="flex items-center text-lg font-bold tracking-tight">
+                    <img src="https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2022/11/26/451507063.jpg" className="h-8 w-8 mr-2 rounded-full" alt="Citilink Logo" />
+                    {tiket.namaMaskapai} - {tiket.kelas}
+                  </p>
                 </div>
-                <div className="p-4 flex justify-end">
-                  <button className="text-white bg-[#00B7C2] hover:bg-[#0f5c60] px-4 py-2 rounded-md" onClick={() => navigate("/detailtiket")}>
+                <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 py-4">
+                  <div className="flex flex-col items-center">
+                    <span className="text-gray-700 dark:text-gray-400 font-semibold">{tiket.waktuKeberangkatan}</span>
+                    <span className="text-gray-700 dark:text-gray-400 font-semibold">{tiket.bandaraAsal}</span>{" "}
+                  </div>
+                  <div className="flex flex-col items-center col-span-2">
+                    <animated.div style={planeAnimation} className="flex flex-col items-center">
+                      <FaPlaneDeparture size={30} className="text-[#00B7C2]" />
+                      <span className="text-gray-700 dark:text-gray-400 font-semibold mt-2 flex items-center">{tiket.durasi}</span>
+                    </animated.div>
+                  </div>
+                  <div className="flex flex-col items-center">
+                    <span className="text-gray-700 dark:text-gray-400 font-semibold">{tiket.waktuKedatangan}</span>
+                    <span className="text-gray-700 dark:text-gray-400 font-semibold">{tiket.bandaraTujuan}</span>
+                  </div>
+                </div>
+                <div className="flex flex-col sm:flex-row items-center sm:justify-end sm:items-center p-4 ">
+                  <span className="text-[#00B7C2] text-lg font-bold mt-2 sm:mt-0">{tiket.harga}</span>
+                  <Link to="/detailtiket" className="block text-center bg-gray-600 hover:bg-gray-800 text-white font-bold text-l py-2 px-4 rounded-md focus:outline-none sm:ml-4 mt-4 sm:mt-0">
                     Lihat
-                  </button>
+                  </Link>
                 </div>
               </div>
-              <div className="flight-ticket-card shadow-lg border-[#00B7C2] border-2 rounded-md  px-4 py-2 text-white text-left font-bold w-full my-3">
-                <h2 className="flex items-center text-lg font-bold tracking-tight text-black dark:text-white">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/f/f5/AirAsia_New_Logo.svg" className="h-8 mr-2" alt="Airasia Logo" />
-                  AirAsia Indonesia
-                </h2>
-                <div className="flex justify-between items-center ">
-                  <div>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold">Ekonomi</h1>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold">Departure: 20:00</h1>
-                    <h1 className="text-gray-700 dark:text-gray-400 font-semibold">Arrival: 22:55</h1>
-                  </div>
-                  <h1 className="text-gray-700 dark:text-gray-400 font-semibold border-b border-gray-700">1j 55mnt</h1>
-                  <h1 className="text-gray-700 dark:text-gray-400 font-bold">RP 972.600/org</h1>
-                </div>
-                <div className="p-4 flex justify-end">
-                  <button className="text-white bg-[#00B7C2] hover:bg-[#0f5c60] px-4 py-2 rounded-md" onClick={() => navigate("/detailtiket")}>
-                    Lihat
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className={`p-4 border border-gray-200 dark:border-gray-700 rounded-md ${activeTab === 1 ? "block" : "hidden"}`}>
-              <h2 className="text-lg font-semibold mb-2">Selasa </h2>
-              <p className="text-sm text-gray-500 dark:text-gray-400">Content 2</p>
-            </div>
-            <div className={`p-4 border border-gray-200 dark:border-gray-700 rounded-md ${activeTab === 2 ? "block" : "hidden"}`}>
-              <h2 className="text-lg font-semibold mb-2">Rabu</h2>
-              <p className="text-sm text-gray-500 dark:text-gray-400">Content 3</p>
-            </div>
-            <div className={`p-4 border border-gray-200 dark:border-gray-700 rounded-md ${activeTab === 3 ? "block" : "hidden"}`}>
-              <h2 className="text-lg font-semibold mb-2">Kamis</h2>
-              <p className="text-sm text-gray-500 dark:text-gray-400">Content 4</p>
-            </div>
-            <div className={`p-4 border border-gray-200 dark:border-gray-700 rounded-md ${activeTab === 4 ? "block" : "hidden"}`}>
-              <h2 className="text-lg font-semibold mb-2">Jumat</h2>
-              <p className="text-sm text-gray-500 dark:text-gray-400">Content 5</p>
-            </div>
-            <div className={`p-4 border border-gray-200 dark:border-gray-700 rounded-md ${activeTab === 5 ? "block" : "hidden"}`}>
-              <h2 className="text-lg font-semibold mb-2">Sabtu</h2>
-              <p className="text-sm text-gray-500 dark:text-gray-400">Content 6</p>
-            </div>
-            <div className={`p-4 border border-gray-200 dark:border-gray-700 rounded-md ${activeTab === 6 ? "block" : "hidden"}`}>
-              <h2 className="text-lg font-semibold mb-2">Minggu</h2>
-              <p className="text-sm text-gray-500 dark:text-gray-400">Content 7</p>
             </div>
           </TabPanel>
         </Tabs>
