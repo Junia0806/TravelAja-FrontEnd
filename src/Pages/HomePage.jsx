a218c7ee684dc0eb92d72a6c86d62cfaebb5cb40
tantriklusi <tantriklusiana@gmail.com>
2024-06-23 19:59:32 +0700

    update alert homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,26 +4,21 @@ import { Modal } from "flowbite-react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { useDispatch } from "react-redux";
-import {
-  setIsLoggedIn,
-  setLogin,
-  setToken,
-} from "../Redux/reducers/authReducers";
+import { setIsLoggedIn, setLogin, setToken } from "../Redux/reducers/authReducers";
 import { getMe } from "../Redux/actions/authActions";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import axios from "axios";
 import FlightPromo from "./PromoPage";
+import Swal from "sweetalert2";
 
 const Home = () => {
   const [formData, setFormData] = useState({
-    // departureAirport: "", //bentuk string
-    departureAirport: null, //bentuk object
+    departureAirport: null,
     arrivalAirport: "",
     departureDate: "",
     passengers: 1,
     seatClass: "",
   });
-  // console.log("form data ", formData);
 
   const location = useLocation();
   const dispatch = useDispatch();
@@ -35,7 +30,6 @@ const Home = () => {
     dispatch(setLogin("Sedang login"));
     dispatch(setIsLoggedIn(true));
     dispatch(setToken(token));
-
     dispatch(getMe(null, null, null));
   }
 
@@ -65,32 +59,21 @@ const Home = () => {
     });
   };
 
-  //use effect untuk bandara
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get(
-          `https://expressjs-develop.up.railway.app/api/v1/airport`
-        );
+        const response = await axios.get(`https://expressjs-develop.up.railway.app/api/v1/airport`);
         setlistBandara(response.data.data);
-        console.log("response.data :>> ", response.data);
-      } catch (error) {
-        console.error("Error", error);
-      }
+      } catch (error) {}
     }
     fetchData();
   }, []);
 
-  // useEffect untuk seat classes
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get(
-          "https://expressjs-develop.up.railway.app/api/v1/seatclasses"
-        );
-        const uniqueSeatClasses = [
-          ...new Set(response.data.data.map((item) => item.seat_class_type)),
-        ];
+        const response = await axios.get("https://expressjs-develop.up.railway.app/api/v1/seatclasses");
+        const uniqueSeatClasses = [...new Set(response.data.data.map((item) => item.seat_class_type))];
         setSeatClasses(uniqueSeatClasses);
       } catch (error) {
         console.error("Error fetching seat classes:", error);
@@ -99,7 +82,6 @@ const Home = () => {
     fetchData();
   }, []);
 
-  // console.log("bandaraAsal", bandaraAsal);
   const items = listbandara.map((e) => ({ ...e, name: e.airport_name }));
 
   const handleOnSearch = (string, results) => {
@@ -110,16 +92,10 @@ const Home = () => {
     console.log(result);
   };
 
-  //data yang dipilih user ambil disini.
   const handleOnSelectAsal = (item) => {
     console.log("item", item);
     setBandaraAsal(item);
-    //string
-    //setFormData({ ...formData, departureAirport: item?.name });
-
-    //object
     setFormData({ ...formData, departureAirport: item });
-
     setOpenModalAsal(false);
   };
 
@@ -137,13 +113,11 @@ const Home = () => {
   const formatResult = (item) => {
     return (
       <div className="flex flex-col">
-        <span className="text-gray-600 text-sm">
-         {item.name}
-        </span>
+        <span className="text-gray-600 text-sm">{item.name}</span>
       </div>
     );
   };
-  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
@@ -152,26 +126,29 @@ const Home = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get(
-        `https://expressjs-develop.up.railway.app/api/v1/search?query=${formData.departureAirport?.name}`
-      );
+      const response = await axios.get(`https://expressjs-develop.up.railway.app/api/v1/search?query=${formData.departureAirport?.name}`);
       setSearchResults(response.data.data);
     } catch (error) {
       console.error("Error fetching search results:", error);
     }
-    console.log(formData);
   };
 
-  //Validasi untuk button cari penerbangan.
   const handleSearch = () => {
-    const { departureAirport, arrivalAirport, departureDate, seatClass } =
-      formData;
+    const { departureAirport, arrivalAirport, departureDate, seatClass } = formData;
     if (!departureAirport || !arrivalAirport || !departureDate || !seatClass) {
-      alert("Silakan Lengkapi Semua Data Sebelum Melakukan Pencarian.");
+      Swal.fire({
+        icon: "warning",
+        title: "Data Tidak Lengkap",
+        text: "Silakan Lengkapi Semua Data Sebelum Melakukan Pencarian.",
+      });
       return;
     }
     if (departureAirport.id === arrivalAirport.id) {
-      alert("Bandara Asal dan Bandara Tujuan Harus Berbeda.");
+      Swal.fire({
+        icon: "error",
+        title: "Data Tidak Valid",
+        text: "Bandara Asal dan Bandara Tujuan Harus Berbeda.",
+      });
       return;
     }
     hasilPencarian();
@@ -184,19 +161,12 @@ const Home = () => {
         <div className="max-w-5xl w-full mx-auto mt-5 p-8 bg-white bg-opacity-20 rounded-lg shadow-lg">
           <div className="flex items-center justify-between mb-6">
             <h2 className="text-3xl font-bold text-center flex-grow text-white">
-              Temukan Penerbangan Terbaikmu{" "}
-              <i className="fa-solid fa-plane-up"></i>
+              Temukan Penerbangan Terbaikmu <i className="fa-solid fa-plane-up"></i>
             </h2>
           </div>
-          <form
-            onSubmit={handleSubmit}
-            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"
-          >
+          <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             <div className="mb-4">
-              <label
-                htmlFor="departureAirport"
-                className="block text-white font-semibold mb-2"
-              >
+              <label htmlFor="departureAirport" className="block text-white font-semibold mb-2">
                 Asal <i className="fa-solid fa-plane-departure"></i>
               </label>
               <input
@@ -211,10 +181,7 @@ const Home = () => {
               />
             </div>
             <div className="mb-4">
-              <label
-                htmlFor="arrivalAirport"
-                className="block text-white font-semibold mb-2"
-              >
+              <label htmlFor="arrivalAirport" className="block text-white font-semibold mb-2">
                 Tujuan <i className="fa-solid fa-plane-arrival"></i>
               </label>
               <input
@@ -229,10 +196,7 @@ const Home = () => {
               />
             </div>
             <div className="mb-4">
-              <label
-                htmlFor="departureDate"
-                className="block text-white font-semibold mb-2"
-              >
+              <label htmlFor="departureDate" className="block text-white font-semibold mb-2">
                 Berangkat
               </label>
               <input
@@ -246,10 +210,7 @@ const Home = () => {
             </div>
             <div>
               <div className="mb-4">
-                <label
-                  htmlFor="seatClass"
-                  className="block text-white font-semibold mb-2"
-                >
+                <label htmlFor="seatClass" className="block text-white font-semibold mb-2">
                   Kelas
                 </label>
                 <select
@@ -269,61 +230,53 @@ const Home = () => {
               </div>
             </div>
             <div className="col-span-1 md:col-span-2 lg:col-span-4 mb-4">
-              <button
-                type="button"
-                onClick={handleSearch}
-                className="block w-full text-center bg-[#00B7C2] text-white font-bold text-xl py-2 px-2 rounded-md hover:bg-gray-800 focus:outline-none"
-              >
-                Cari Penerbangan{" "}
+              <button type="button" onClick={handleSearch} className="block w-full text-center bg-[#00B7C2] text-white font-bold text-xl py-2 px-2 rounded-md hover:bg-gray-800 focus:outline-none">
+                Cari Penerbangan
                 <i className="fa-solid fa-magnifying-glass ml-2"></i>
               </button>
             </div>
           </form>
         </div>
 
-        {/* modal untuk bandara asal */}
         <Modal show={openModalAsal} onClose={() => setOpenModalAsal(false)}>
-          <Modal.Header>Cari Bandara Keberangkatan</Modal.Header>
+          <Modal.Header className="p-4 bg-teal-500">
+            <h2 className="text-xl text-white font-bold">Pencarian Bandara Keberangkatan</h2>
+          </Modal.Header>
           <Modal.Body>
-            <form className="mb-8">
-              <div className="">
-                <ReactSearchAutocomplete
-                  items={items}
-                  onSearch={handleOnSearch}
-                  onHover={handleOnHover}
-                  onSelect={handleOnSelectAsal}
-                  onFocus={handleOnFocus}
-                  autoFocus
-                  formatResult={formatResult}
-                />
-              </div>
-            </form>
+            <div className="mb-16">
+              <ReactSearchAutocomplete
+                items={items}
+                onSearch={handleOnSearch}
+                onHover={handleOnHover}
+                onSelect={handleOnSelectAsal}
+                onFocus={handleOnFocus}
+                autoFocus
+                formatResult={formatResult}
+                inputClass="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Cari Nama Bandara"
+              />
+            </div>
           </Modal.Body>
         </Modal>
 
-        {/* modal untuk bandara tujuan */}
         <Modal show={openModalTujuan} onClose={() => setOpenModalTujuan(false)}>
-          <Modal.Header>Pencarian Bandara</Modal.Header>
+          <Modal.Header className="p-4 bg-teal-500">
+            <h2 className="text-xl text-white font-bold">Pencarian Bandara Tujuan</h2>
+          </Modal.Header>
           <Modal.Body>
-            <form>
-              <div className="mb-4">
-                <label
-                  htmlFor="searchQuery"
-                  className="block text-gray-700 font-semibold mb-2"
-                >
-                  Nama Bandara
-                </label>
-                <ReactSearchAutocomplete
-                  items={items}
-                  onSearch={handleOnSearch}
-                  onHover={handleOnHover}
-                  onSelect={handleOnSelectTujuan}
-                  onFocus={handleOnFocus}
-                  autoFocus
-                  formatResult={formatResult}
-                />
-              </div>
-            </form>
+            <div className="mb-16">
+              <ReactSearchAutocomplete
+                items={items}
+                onSearch={handleOnSearch}
+                onHover={handleOnHover}
+                onSelect={handleOnSelectTujuan}
+                onFocus={handleOnFocus}
+                autoFocus
+                formatResult={formatResult}
+                inputClass="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Cari Nama Bandara"
+              />
+            </div>
           </Modal.Body>
         </Modal>
       </div>
